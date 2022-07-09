30c9be0cc448bfff1c9f1e7ed1a29c4e096d6dca
miriam schlindwein <schlindwein@hallowelt.com>
2022-07-09 17:52:41 +0200

    Fix prefix
    ... qunit test for upload file params
    
    ERM29017
    
    [4.2.x]
    
    Change-Id: I1a193f5aeb24fb23d94933c3f6f64bb7d3ab5a1a

---
+++ resources/ParamsProcessor.js
@@ -0,0 +1,48 @@
+window.enhancedUpload = window.enhancedUpload || {};
+
+enhancedUpload.ParamsProcessor = function () {};
+
+OO.initClass( enhancedUpload.ParamsProcessor );
+
+// allows processor to create widget for upload params
+enhancedUpload.ParamsProcessor.prototype.init = function () {};
+
+// allows processor to set namespace values
+// eslint-disable-next-line no-unused-vars
+enhancedUpload.ParamsProcessor.prototype.setNamespaceValue = function ( prefix ) {};
+
+// allows modification for upload params
+// eslint-disable-next-line no-unused-vars
+enhancedUpload.ParamsProcessor.prototype.getParams = function ( params, item, skipOption ) {
+	if ( params.filename === item.name && params.prefix !== '' ) {
+		// eslint-disable-next-line no-underscore-dangle
+		this._getParamFileName( params, params.prefix );
+	}
+
+	return params;
+};
+
+// eslint-disable-next-line no-underscore-dangle
+enhancedUpload.ParamsProcessor.prototype._getParamFileName = function ( params, prefix ) {
+	var filename = params.filename;
+	var prefixPartOne = '';
+	var prefixParts = prefix.split( ':' );
+
+	if ( prefixParts.length > 1 ) {
+		prefixPartOne = prefixParts[ 0 ];
+		prefixParts.splice( 0, 1 );
+	}
+
+	var prefixStub = prefixParts.join( ':' );
+	prefixStub = prefixStub.replace( ':', '_' );
+
+	if ( prefixPartOne.length > 0 ) {
+		prefixStub = prefixPartOne + '_' + prefixStub;
+	}
+
+	if ( prefixStub === filename.slice( 0, prefixStub.length ) ) {
+		prefixStub = '';
+	}
+
+	params.filename = prefixStub + filename;
+};
