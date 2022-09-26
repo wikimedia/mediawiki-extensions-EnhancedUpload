463992e903057f1e378344cec1bcd6936cac5838
Hamish Slater <hamishslater@hotmail.com>
2022-09-26 11:02:15 +0200

    Fix deprecated calls to 'WikiPage::factory'
    'MediaWikiServices::getWikiPageFactory' requires REL1_39
    
    ERM29915
    
    [master]
    
    Change-Id: I80d4f415dfa3f4c208f771dd115703e685544ef0
    (cherry picked from commit 5018fe5d3c3f98d4006ddf9f020089eef1268c1b)

--- src/Rest/SetAttachments.php
+++ src/Rest/SetAttachments.php
@@ -16,7 +16,6 @@ use Title;
 use TitleFactory;
 use User;
 use Wikimedia\ParamValidator\ParamValidator;
-use WikiPage;
 use WikitextContent;
 
 class SetAttachments extends SimpleHandler {
@@ -127,12 +126,8 @@ class SetAttachments extends SimpleHandler {
 	 * @return string
 	 */
 	private function getWikiText( $title ) {
-		if ( method_exists( MediaWikiServices::class, 'getWikiPageFactory' ) ) {
-			// MW 1.36+
-			$wikiPage = MediaWikiServices::getInstance()->getWikiPageFactory()->newFromTitle( $title );
-		} else {
-			$wikiPage = WikiPage::factory( $title );
-		}
+		$wikiPage = MediaWikiServices::getInstance()->getWikiPageFactory()
+			->newFromTitle( $title );
 		if ( !$wikiPage ) {
 			return Status::newFatal( 'Invalid WikiPage' );
 		}
@@ -152,12 +147,8 @@ class SetAttachments extends SimpleHandler {
 	 * @return Status
 	 */
 	private function savePage( $title, WikitextContent $content, $user ): Status {
-		if ( method_exists( MediaWikiServices::class, 'getWikiPageFactory' ) ) {
-			// MW 1.36+
-			$wikiPage = MediaWikiServices::getInstance()->getWikiPageFactory()->newFromTitle( $title );
-		} else {
-			$wikiPage = WikiPage::factory( $title );
-		}
+		$wikiPage = MediaWikiServices::getInstance()->getWikiPageFactory()
+			->newFromTitle( $title );
 		$revision = null;
 		try {
 			$updater = $wikiPage->newPageUpdater( $user );
