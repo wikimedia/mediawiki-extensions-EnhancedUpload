5ced8f380fd797d17774c4a7a0148637d8668aa8
Mschlindwein <schlindwein@hallowelt.com>
2025-04-16 09:11:39 +0200

    Add loading skeleton
    ERM31194
    
    [5.x]
    
    Change-Id: Ida1d0b900ce8b33232e5960737c53a15bb89f512

--- resources/enhancedUpload.js
+++ resources/enhancedUpload.js
@@ -13,6 +13,10 @@ $( () => {
 			destFilename: destFilename
 		} ).$element
 	);
+	if ( $( document ).find( '#enhancedUpload-skeleton-cnt' ) ) {
+		// eslint-disable-next-line no-jquery/no-global-selector
+		$( '#enhancedUpload-skeleton-cnt' ).empty();
+	}
 
 	// eslint-disable-next-line no-jquery/no-global-selector
 	$( '#enhancedUpload-container' ).addClass( 'enhancedUpload-widget' );
--- src/Special/EnhancedUpload.php
+++ src/Special/EnhancedUpload.php
@@ -3,12 +3,20 @@
 namespace MediaWiki\Extension\EnhancedUpload\Special;
 
 use MediaWiki\Html\Html;
+use MediaWiki\Html\TemplateParser;
 use MediaWiki\SpecialPage\SpecialPage;
 
 class EnhancedUpload extends SpecialPage {
 
+	/** @var TemplateParser */
+	protected $templateParser;
+
 	public function __construct() {
 		parent::__construct( 'EnhancedUpload', 'upload' );
+
+		$this->templateParser = new TemplateParser(
+			dirname( __DIR__, 2 ) . '/resources/templates'
+		);
 	}
 
 	/**
@@ -19,16 +27,33 @@ class EnhancedUpload extends SpecialPage {
 		$this->setHeaders();
 
 		$this->checkPermissions();
-
-		$this->getOutput()->addModules( "ext.enhancedUpload" );
-
 		$this->getOutput()->enableOOUI();
+		$this->buildSkeleton();
+		$this->getOutput()->addModules( "ext.enhancedUpload" );
 
 		$this->getOutput()->addHTML(
 			Html::element( 'div', [
 				'id' => 'enhancedUpload-container'
 			] )
 		);
+	}
+
+	/**
+	 *
+	 * @return void
+	 */
+	protected function buildSkeleton() {
+		$this->getOutput()->addModuleStyles( [ 'ext.enhancedUpload.special.skeleton.styles' ] );
+		$skeleton = $this->templateParser->processTemplate(
+			'upload-skeleton',
+			[]
+		);
+		$skeletonCnt = Html::openElement( 'div', [
+			'id' => 'enhancedUpload-skeleton-cnt'
+		] );
+		$skeletonCnt .= $skeleton;
+		$skeletonCnt .= Html::closeElement( 'div' );
+		$this->getOutput()->addHTML( $skeletonCnt );
 	}
 
 	/**
